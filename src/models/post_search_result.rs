64bfd39cc993d75b7ae7a2af8f0f5494cf3aa600
Pocket7878 <poketo7878@gmail.com>
2016-06-26 16:56:48 +0900

    Replace docbase_cli with docbase_io

--- src/models/post_search_result.rs
+++ src/models/post_search_result.rs
@@ -1,4 +1,4 @@
-// docbase_cli -- docbase.io cli written in Rust.
+// docbase_io -- docbase.io cli written in Rust.
 // Copyright 2016 Pocket7878 <poketo7878@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
